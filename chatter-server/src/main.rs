132df216c8f1ff644ee48328772ce24638dd8bb7
jeff loos <jeffloos@jeffs-mbp.attlocal.net>
2021-01-06 20:45:39 -0800

    various syntactical fixes

--- chatter-server/src/main.rs
+++ chatter-server/src/main.rs
@@ -1,7 +1,8 @@
-use actix_web::{ App, Error, HttpResponse, HttpServer, web ;
-use actix_web::http::{StatusCode};
+use actix_web::{ App, Error, HttpResponse, HttpServer, Responder, web };
+use actix_web::http::StatusCode;
+use mysql::params;
+use mysql::Pool;
 use serde::{ Deserialize, Serialize };
-
 
 #[derive(Deserialize)]
 pub struct NewUserPayload {
@@ -18,7 +19,7 @@ async fn home() -> Result<HttpResponse, Error> {
 }
 
 
-fn insert_new_user(conn: &mysql::PooledConn, new_user: User) -> Result<()> {
+fn insert_new_user(conn: &mysql::PooledConn, new_user: NewUserPayload) -> Result<()> {
     conn.exec_batch(
         r"INSERT INTO users (username)
           VALUES (:username)", new_user.iter().map(|u| params! {
@@ -31,7 +32,7 @@ fn insert_new_user(conn: &mysql::PooledConn, new_user: User) -> Result<()> {
 async fn new_user_index(pool: web::Data<mysql::Pool>, body: web::Json<NewUserPayload>) -> impl Responder {
     let conn = pool.get_conn();
 
-    let user = web::block(move || actions::insert_new_user(&conn, &body))
+    let user = web::block(move || insert_new_user(&conn, &body))
         .await
         .map_err(|e| {
             eprintln!("{}", e);
@@ -43,19 +44,19 @@ async fn new_user_index(pool: web::Data<mysql::Pool>, body: web::Json<NewUserPay
 }
 
 #[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<(), Error> {
     let url = "mysql://root:password@db:3306/chatter_db";
 
     let pool = Pool::new(url)?;
 
     HttpServer::new(move || {
         App::new::data(pool.clone())
-            .resource("/new_user", web::post().to(new_user_index)
+            .resource("/new_user", web::post().to(new_user_index))
             .route("/", web::get().to(home))
     })
     .bind("127.0.0.1:8088")?
     .run()
-    .await
+    .await?
 
     Ok()
 }
